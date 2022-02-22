3452365d57bf67e11974736db0eb121d1dec873b
Aayush <ayxsth@gmail.com>
2022-02-22 23:00:06 +0545

    refactored codes

--- LoginApp/src/main/java/com/iims/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.iims.dao;
-
-import com.iims.models.User;
-
-import java.sql.SQLException;
-
-public interface UserDao {
-    User findOne(String username, String password) throws SQLException, ClassNotFoundException;
-}
